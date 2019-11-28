634121a82709e84f40a06a9aff88e3fed2c5f88c
Nikos Alexandris <nik@nikosalexandris.net>
2019-11-28 12:56:28 +0100

    Update reading of labels for mode='train'

--- robosat_pink/loaders/TestRBG.py
+++ robosat_pink/loaders/TestRBG.py
@@ -40,7 +40,7 @@ class TestRGB(torch.utils.data.Dataset):
         if self.mode == "train":
             path = os.path.join(self.root, "labels")
             #MODIFIED Change list of label masks to single class name.
-            self.tiles["labels"] = [(tile, label) for tile, path,label in tiles_from_dir_NOGIS(path)]
+            self.tiles["labels"] = [(number, label) for number, label in enumerate_image_labels(path, 'jpg')]
             self.tiles["labels"].sort(key=lambda tile: tile[0])
 
         assert len(self.tiles), "Empty Dataset"
