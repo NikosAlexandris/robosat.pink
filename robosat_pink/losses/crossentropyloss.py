2a44f1dde20199ca3e29f8f6ffa06c772f2e6c59
Nikos Alexandris <nik@nikosalexandris.net>
2019-11-29 15:35:31 +0000

    Update crossentropyloss.py

--- robosat_pink/losses/crossentropyloss.py
+++ robosat_pink/losses/crossentropyloss.py
@@ -8,6 +8,7 @@ class CrossEntropyLoss(nn.Module):
         super().__init__()
         self.loss=nn.CrossEntropyLoss()
     def forward(self, inputs, targets, config):
-        print(inputs.shape)
-        print(targets.shape)
+        zeros=torch.zeros(targets.shape).long().cuda()
+        ones=torch.ones(targets.shape).long().cuda()
+        targets = torch.where(targets == 17,ones,zeros)
         return self.loss.forward(inputs,targets)
